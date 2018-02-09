5721a54e33bae8b08e8f89f4ca64173546c7ac79
Vuzeta95 <machnio95@o2.pl>
2018-02-09 13:47:11 +0100

    a

--- js/scroll.js
+++ js/scroll.js
@@ -3,4 +3,17 @@
       $('html, body').animate({scrollTop: $('#about .admission').offset().top }, 'slow');
       return false;
     });
-  },1000);
+  },3000);
+
+ $('document').ready(function() {
+    $('.slide-section').click(function (e) {
+        
+        var linkHref = $(this).attr('href');
+        
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        });
+        e.preventDefault();
+    })
+     
+ },3000);
