e0f1b79e4930ab4139b99d27e203c6a17ec7fece
Vuzeta95 <machnio95@o2.pl>
2018-02-11 20:19:23 +0100

    finally?
    czy cos jeszcze?

---
+++ js/scrollspy.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop - 130;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    
+      
+      for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
